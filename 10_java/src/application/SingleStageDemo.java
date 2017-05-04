9b26ef4c332a74d9467647dd660bbd3cef43d55c
borovan <borovan@microstep-mis.com>
2017-05-04 13:25:30 +0200

    desiata prednaska, cvicenie

---
+++ 10_java/src/application/SingleStageDemo.java
@@ -0,0 +1,27 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class SingleStageDemo extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Button btn = new Button("Press me !");
+			Pane root = new Pane(btn);
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
